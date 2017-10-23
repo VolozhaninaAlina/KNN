635a35a5ba731fca7d1be83eb0080f45164b9f7a
VolozhaninaAlina <32459374+VolozhaninaAlina@users.noreply.github.com>
2017-10-24 01:08:26 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,48 @@
+print(__doc__)
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn import neighbors, datasets
+
+n_neighbors = 15
+
+# Импортируем некоторых данных для работы с ними
+iris = datasets.load_iris()
+
+# Принимаем только первые две функции
+X = iris.data[:, :2]
+y = iris.target
+
+h = .02  # Задаем размер шага
+
+# Создаем цветной карты
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+
+for weights in ['uniform', 'distance']:
+    # Создаем экземпляр Классификатора соседей 
+    clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
+    clf.fit(X, y)
+
+    # Выделяем границу решения, назначая цвет каждой точке [x_min, x_max] x [y_min, y_max].
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Отображаем результат 
+    Z = Z.reshape(xx.shape)
+    plt.figure()
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+    # Параметры процесса обучения
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold,
+                edgecolor='k', s=20)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    plt.title("3-Class classification (k = %i, weights = '%s')"
+              % (n_neighbors, weights))
+
+plt.show()
